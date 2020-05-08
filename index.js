b17e398961c44d6d13bab07bdfd6db86723315d2
Andrey Zinoviev <dronis_dronis@mail.ru>
2020-05-08 03:00:16 +0300

    Добавлено изменение пользователя через всплывающее окно

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ let outerZone = document.querySelector('.popup');
 let author = document.querySelector('.profile-info__heading');
 let subtitle = document.querySelector('.profile-info__subtitle');
 let authorPopup = document.querySelector('.user-edit__author');
-let subtitlePopup = document.querySelector('.user-edit__subtitle')
+let subtitlePopup = document.querySelector('.user-edit__subtitle');
+let submitUserChanges = document.querySelector('.user-edit__button');
+let editForm = document.querySelector('.user-edit');
+let data;
 userEditButton.addEventListener('click', function() {
     popup.classList.add('popup-window_opened');
     authorPopup.value = author.textContent;
@@ -14,6 +17,19 @@ userEditButton.addEventListener('click', function() {
 closeWindow.addEventListener('click', function() {
     popup.classList.remove('popup-window_opened');
 })
-outerZone.addEventListener('click', function() {
+// outerZone.addEventListener('click', function() {
+//     popup.classList.remove('popup-window_opened');
+// })
+// function submitForm(event) {
+//     event.preventDefault
+// }
+function formSubmit(event) {
+    event.preventDefault();
     popup.classList.remove('popup-window_opened');
-})
+    author.textContent = authorPopup.value;
+    subtitle.textContent = subtitlePopup.value;
+    console.log(author.textContent);
+    console.log(subtitle.textContent);
+}
+editForm.addEventListener('submit', formSubmit)
+
