7b1e9b9c5814e9ada5722e2346ae2a24589cf1ec
Andrey Zinoviev <dronis_dronis@mail.ru>
2020-05-23 22:14:23 +0300

    добавить карточку

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ let authorPopup = document.querySelector('.popup__input_order_first');
 let subtitlePopup = document.querySelector('.popup__input_order_second');
 const submitUserChanges = document.querySelector('.popup__edit-button');
 const editForm = document.querySelector('.popup__user-edit');
+const addCardButton = document.querySelector('.profile__add-button');
+const popupAddCard = document.querySelector('.popup_addCard');
+const closeWindowAddCard = document.querySelector('.popup__close_type_addCard');
+const addCardForm = document.querySelector('.popup__addCard-form');
+let cardName = document.querySelector('.popup__input_place-name');
+let cardPicture = document.querySelector('.popup__input_image-link');
+const cardTemplate = document.querySelector('#card-container');
+const cardsList = document.querySelector('.elements__list');
 const initialCards = [
     {
         name: "Карачаевск",
@@ -36,12 +44,14 @@ const initialCards = [
 
 function openPopup() {
     popup.classList.add('popup_opened');
-    authorPopup.value = author.textContent;
-    subtitlePopup.value = subtitle.textContent
 }
 
-function closePopup() {
-    popup.classList.remove('popup_opened');
+function closePopup(element) {
+    element.classList.remove('popup_opened');
+}
+
+function openAddPopup() {
+    popupAddCard.classList.add('popup_opened');
 }
 
 function formSubmit(event) {
@@ -51,17 +61,32 @@ function formSubmit(event) {
     subtitle.textContent = subtitlePopup.value;
 }
 
-function togglePopup() {
-    if(popup.classList.contains('popup_opened')) {
-        closePopup();
+function togglePopup(event) {
+    let element = event.target.parentElement.parentElement.parentElement;
+    if(element.classList.contains('popup_opened')) {
+        closePopup(element);
     } else {
         openPopup();
+        authorPopup.value = author.textContent;
+        subtitlePopup.value = subtitle.textContent;
+    }
+}
+function toggleAddPopup(event) {
+    let element = event.target.parentElement.parentElement.parentElement;
+    if(element.classList.contains('popup_opened')) {
+        closePopup(element);
+    } else {
+        openAddPopup();
     }
 }
 
 userEditButton.addEventListener('click', togglePopup);
 closeWindow.addEventListener('click', togglePopup);
 editForm.addEventListener('submit', formSubmit);
+addCardButton.addEventListener('click', toggleAddPopup);
+closeWindowAddCard.addEventListener('click', toggleAddPopup);
+
+
 
 let cards = Array.from(document.querySelectorAll('.elements__element'));
 let cardsNames = cards.map(card => {
@@ -76,3 +101,15 @@ cardsNames.forEach((el, index, array) => {
 cardsLinks.forEach((el, index, array) => {
     array[index].src = initialCards[index].link;
 })
+
+function addCard(event) {
+    event.preventDefault();
+    const cardContainer = cardTemplate.content;
+    const cardElement = cardContainer.cloneNode(true);
+    cardElement.querySelector('.elements__element-photo').src = cardPicture.value;
+    cardElement.querySelector('.elements__element-text').textContent = cardName.value;
+    cardsList.prepend(cardElement);
+    closePopup(event.target.parentElement.parentElement);
+}
+
+addCardForm.addEventListener('submit', addCard);
